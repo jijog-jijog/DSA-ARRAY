98fb0017063c4e4aa581b058b6fbf178015242ed
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-06-23 11:19:30 +0530

    Add initial implementation of palindrome checker

---
+++ palindrome.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class palindrome {
+    public static void main(String[] args) {
+        System.out.print("Enter the number want to check : ");
+        Scanner scan = new Scanner(System.in);
+        long val = scan.nextLong();
+        scan.close();
+        long rev = 0;
+        long str = 0;
+        long chk = val;
+
+        while (val > 0) {
+            rev = val%10;
+            val = val/10;
+            str = (str*10 + rev);
+        }
+        System.out.println(str);
+        if (chk == str) {
+            System.out.print("The given number is palindrome");
+        }
+        else{
+            System.out.println("Not palindrome");
+        }
+    }
+}
