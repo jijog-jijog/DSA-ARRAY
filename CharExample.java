ae7504a9569644a1c5fd37e3ce3973866aed5bcf
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-06-23 09:04:06 +0530

    Fix character frequency counter to correctly handle lowercase letters

--- CharExample.java
+++ CharExample.java
@@ -10,13 +10,13 @@ public class CharExample {
         int arr[] = new int[26];
         for(int i=0; i<value.length(); i++){
             char ch = value.charAt(i);
-            arr[ch - 'A']++;
+            arr[ch - 'a']++;   //we have defined array length is int value so that will -65
 
         
         }
 
         for(int j=0; j<26; j++){
-            char mh = (char)(j+ 'A');
+            char mh = (char)(j+ 'a');
             System.out.println(mh +" -----> " + arr[j] );
         }
 
